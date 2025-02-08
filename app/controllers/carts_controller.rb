5f217dba7acc0ca85cfb92f6e53875d370d0405c
Caue Massi <cauemassi@gmail.com>
2025-02-08 03:22:27 -0300

    Update routes and CartsController -  Add items in cart edpoint implemented

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,45 @@
 class CartsController < ApplicationController
-  ## TODO Escreva a lógica dos carrinhos aqui
+  include ActionController::Cookies
+
+  before_action :set_cart
+
+  def create
+
+  end
+
+  def show
+  end
+
+  def add_items
+    @cart_item = CartItem.find_by(cart: @cart, product: params[:product_id])
+
+    if @cart_item
+      @cart_item.quantity += params[:quantity]
+
+      @cart_item.save!
+    else
+      render json: "error: Product not in Cart to be add. Please first add Product"
+    end
+  end
+
+  def delete_items
+
+  end
+
+  private
+
+  def set_cart
+    cart_id = cookies[:cart_id]
+
+    if cart_id
+      @cart = Cart.find(cart_id)
+    else
+      @cart = Cart.create!(total_price: 0)
+      cookies[cart_id] = @cart.id
+    end
+  end
+
+  def product_params
+    params.require(:cart).permit(:product_id, :quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+
   resources :products
-  get "up" => "rails/health#show", as: :rails_health_check
+
+  resource :cart, only: [:show, :create] do
+    post 'add_items', on: :collection
+    delete ':product_id', to: 'carts#delete_items', as: :delete_item
+  end
+
+  get "up", to: "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
 end
